08bac1c3333d6b3fd6a71537df796581afdb134d
Chris <chrisjokim@gmail.com>
2020-08-24 15:19:03 -0400

    Added config files to set up the AWS Cognito user pool for authentication on our web application. Code Pipeline w/ an S3 bucket has been configured in the back end.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_ntZ86inzC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2dv3rp1rb5pjb63dvi6q7n9riq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
