7f32d41106b897b79b5424e28360ad675806db38
Chris <chrisjokim@gmail.com>
2020-08-25 12:15:26 -0400

    Updated the Config.js file to now have an invoke URL to our API Gateway.

--- js/dist/config.dev.js
+++ js/dist/config.dev.js
@@ -10,7 +10,7 @@ window._config = {
 
   },
   api: {
-    invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl: 'https://bq6orn9ttk.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
 
   }
 };
